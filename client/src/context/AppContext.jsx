694bb41c3f4cf4d7b61d41f4f0b4c296e7506c05
iamRaphealOgunnaike <107220283+iamRaphealOgunnaike@users.noreply.github.com>
2025-04-07 07:45:27 -0700

    first commit in month of Apiril 2025

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -97,7 +97,7 @@ export const AppContextProvider = (props) => {
   const calculateNoOfLectures = (course) =>{
     let totalLectures = 0;
     course.courseContent.forEach(chapter =>{
-      if(Array.isArray(chapterContent)){
+      if (Array.isArray(chapter.chapterContent)){
         totalLectures += chapter.chapterContent.length
       }
     });
