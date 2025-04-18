28c3ad2ef676d232a3ed6ee441764091d827f66a
iamRaphealOgunnaike <107220283+iamRaphealOgunnaike@users.noreply.github.com>
2025-04-18 07:23:01 -0700

    first commit for server

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,6 +1,6 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 
-// connect to the MongoDB
+// connect to the MongoDB database
 
 const connectDB = async () => {
     mongoose.connection.on('connected', ()=> console.log('Database Connected'))
